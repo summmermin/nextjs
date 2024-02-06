a1316b93ef4d3ff8dd8602adff3ebe4696bb19ba
summer <110873921+summmermin@users.noreply.github.com>
2024-02-06 16:11:31 +0900

    [ADD] 글 삭제 기능

--- src/Contorl.tsx
+++ src/Contorl.tsx
@@ -1,9 +1,11 @@
 "use client"
 import Link from 'next/link';
-import {useParams} from 'next/navigation';
+import {useParams, useRouter} from 'next/navigation';
+import localFont from 'next/dist/compiled/@next/font/dist/local';
 
 export default function Contorl() {
     const params = useParams()
+    const router = useRouter()
     const id = params.id
     return (
         <ul>
@@ -11,7 +13,16 @@ export default function Contorl() {
             {
                 id ? <>
                     <li><Link href={`/update/${id}`}>Update</Link></li>
-                    <li><input type="button" value="delete"/></li>
+                    <li><input type="button" value="delete" onClick={() => {
+                        const options = {method: 'DELETE'}
+                        fetch('http://localhost:9999/topics/' + id, options)
+                            .then(resp => resp.json())
+                            .then(result => {
+                                router.push('/')
+                                router.refresh()
+                                console.log(result)
+                            })
+                    }}/></li>
                 </> : null
             }
         </ul>
